eca454195ac026fd61c88aab15ce50b58a4f9491
srikar-rao <67662872+srikar-rao@users.noreply.github.com>
2024-08-17 01:32:45 -0400

    Revert "segragate tests"
    This reverts commit 64208e3ecf45621a1077dc49e4559da706925915.

--- src/test/java/gcp/hello/world/app/repositorytest/TodoRepositoryIntegrationTest.java
+++ src/test/java/gcp/hello/world/app/repositorytest/TodoRepositoryIntegrationTest.java
@@ -1,4 +1,4 @@
-package gcp.hello.world.app.test.repositorytest;
+package gcp.hello.world.app.repositorytest;
 
 import gcp.hello.world.app.entity.Todo;
 import gcp.hello.world.app.repostiory.TodoRepository;
